f7919c6e86dcf405c51fdb6ff60df272c6c97730
PiotrSmoreda <74731729+PiotrSmoreda@users.noreply.github.com>
2023-10-05 18:26:42 +0200

    zad4.php

--- 04_10_2023/zad4.php
+++ 04_10_2023/zad4.php
@@ -12,10 +12,8 @@
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Pobierz wartość liczby z formularza
     $liczba = $_POST['liczba'];
 
-    // Sprawdź, czy liczba jest mniejsza, większa lub równa zero
     if ($liczba < 0) {
         echo "Podana liczba {$liczba} jest mniejsza od zera.";
     } elseif ($liczba === '0' || $liczba === 0) {
