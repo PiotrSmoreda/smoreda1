d7a71914b2739ed82b34fa2e98f091344c89afda
PiotrSmoreda <74731729+PiotrSmoreda@users.noreply.github.com>
2023-10-05 18:25:56 +0200

    zad1.php

--- 04_10_2023/zad1.php
+++ 04_10_2023/zad1.php
@@ -12,10 +12,9 @@
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Pobierz wartość liczby z formularza
+
     $liczba = $_POST['liczba'];
 
-    // Sprawdź parzystość liczby
     if ($liczba % 2 == 0) {
         echo "Podana liczba {$liczba} jest liczbą parzystą.";
     } else {
