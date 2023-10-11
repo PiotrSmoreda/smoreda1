114eb824cfd7a3e019ba0fa6907571d414afb201
ZiajaNieLubieCie4 <kelme.majers@gmail.com>
2023-10-11 14:59:00 +0200

    strona

--- 11_10_2023/pogoda.php
+++ 11_10_2023/pogoda.php
@@ -2,10 +2,22 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prognoza pogody Wrocław</title>
+    <link rel="stylesheet" href="styl2.css" />
 </head>
 <body>
-    
+   <div id="banner1">
+    <img src="logo.png" alt="meteo" />
+</div>
+<div id="banner2">
+    <h1>Prognoza dla Wrocławia</h1>
+</div>
+<div id="banner3">
+    <p>maj, 2019 r.</p>
+</div>
+<div id="glowny">
+    <table>
+        <tr>
+
 </body>
 </html>
