2a724c3d189fc89000c80901c372599ce9bdc106
PiotrSmoreda <piter.85magnum@gmail.com>
2023-10-18 14:41:17 +0200

    1

--- 18.10.2023/zadanie2.php
+++ 18.10.2023/zadanie2.php
@@ -3,10 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zadanie2</title>
+    <title>Zadanie 2</title>
+    <?php 
+   
+    ?>
 </head>
 <body>
-    <h1>Formularz</h1>
-    <form action="pokaz_wiersz.php
+    <form action="" method="post">
+    ID:<input type="text" name="ID" id="ID" requested>
+        Nazwa:<input type="text" name="Nazwa" id="Nazwa" requested>
+        <input type="submit" value="Sprawdź" >
+        
+        <?php
+            $host = "localhost";
+            $user = "root";
+            $pass = "";
+            $db = "baza";
+
+                $polaczenie = mysqli_connect($host, $user, $pass, $db);
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $Nazwa = $_POST['Nazwa'];
+                    $ID = $_POST['ID'];
+                    
+
+                $zapytania = "SELECT * FROM baza WHERE id = '$ID' AND nazwa = '$Nazwa';";
+                $wynik = mysqli_query($polaczenie, $zapytania);
+
+                if($wynik->num_rows > 0)
+                {
+                    $row = $wynik->fetch_assoc();
+                    echo "<br>" . "ID to: " . $row['ID']. "<br>" .  "Nazwa: " . $row['nazwa'];
+                }
+                else {
+                    echo "<br>" ."Brak danych dla podanych ID i nazwy";
+                }
+
+                
+            }
+                ?>
+    </form>
+    
 </body>
 </html>
