5b759bcc4732cc2d9cfb538ee50317e7c755a36d
PiotrSmoreda <piter.85magnum@gmail.com>
2023-10-18 14:48:54 +0200

    1

---
+++ 18.10.2023/zadanie3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 3</title>
+    <?php 
+   
+    ?>
+</head>
+<body>
+    <form action="" method="post">
+    ID:<input type="text" name="ID" id="ID" requested>
+        Nazwa:<input type="text" name="Nazwa" id="Nazwa" requested>
+        <input type="submit" value="Sprawdź" >
+        
+     
+    </form>
+    
+</body>
+</html>
