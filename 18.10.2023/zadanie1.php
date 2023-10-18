e8d57e8bad8ec007bebe5c144ca6ec6013d2456f
PiotrSmoreda <piter.85magnum@gmail.com>
2023-10-18 13:02:15 +0200

    1

---
+++ 18.10.2023/zadanie1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 1</title>
+</head>
+<body>
+    <h1>Dodaj nowy wiersz</h1>
+    <form action="dodaj_wiersz.php" method="post">
+        Kolumna 1: <input type="text" name="kolumna1"><br>
+        Kolumna 2: <input="submit" value="Dodaj wiersz">
+</form>
+    
+</body>
+</html>
