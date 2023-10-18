0aa17b1e1019fccdfa9c86f8fe17f018aa764b99
PiotrSmoreda <piter.85magnum@gmail.com>
2023-10-18 14:03:17 +0200

    10

--- 18.10.2023/zadanie1.php
+++ 18.10.2023/zadanie1.php
@@ -7,9 +7,22 @@
 </head>
 <body>
     <h1>Dodaj nowy wiersz</h1>
-    <form action="baza2.sql" method="post">
+    <form action="baza.php" method="post">
         Kolumna 1: <input type="text" name="kolumna1"><br>
-        Kolumna 2: <input="submit" value="Dodaj wiersz">
+    
+        <?php
+        $host = "localhost";
+        $user = "root";
+        $pass = "";
+        $db = "zadanie1";
+        $polaczenie=mysqli_connect($host, $user, $pass, $db);
+        if ($_SERVER["REQUEST_METHOD"] == "POST")   {
+            $wiersz = $_POST['wiersz'];
+            $zapytania = "INSERT INTO zadanie1 VALUES ('$wiersz')";
+
+            $wynik = mysqli_query($polaczenie, $zapytania); 
+        }
+        ?>
 </form>
     
 </body>
---
+++ 18.10.2023/zadanie2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie2</title>
+</head>
+<body>
+    <h1>Formularz</h1>
+    <form action="pokaz_wiersz.php
+</body>
+</html>
