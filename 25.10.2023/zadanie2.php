57c1ef3a5e77b677bbee8de7cc0a350733adb08c
PiotrSmoreda <piter.85magnum@gmail.com>
2023-10-25 15:05:46 +0200

    1

---
+++ 25.10.2023/zadanie2.php
@@ -0,0 +1,24 @@
+<?php
+if(!isset ($_COOKIE['username'])) {
+    echo "Cookie named ' is not set!";
+} else {
+    echo "Value is: ". $_COOKIE['username'];
+}
+?>
+
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UFT-8">
+    <meta name="viewport" concent="width=device-width, initial-scale=1.0">
+    <title>COOKIE</title>
+</head>
+</html>
+
+
+<html>
+<body>
+
+
