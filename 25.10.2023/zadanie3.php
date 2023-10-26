23f49a3cde34a89b62db9ba0deb71c6dcd848ed7
PiotrSmoreda <piter.85magnum@gmail.com>
2023-10-26 14:01:24 +0200

    1

--- 25.10.2023/zadanie3.php
+++ 25.10.2023/zadanie3.php
@@ -1,1 +1,5 @@
-erqweer321
+<?php
+$newusername = "NowaNazwaUzytkownika";
+setcookie("username", $newusername, time() + 3600, "/"); 
+echo "Plik cookie 'username' został zaktualizowany. Nowa wartość: " . $_COOKIE['username'];
+?>
