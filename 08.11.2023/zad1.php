1bc1e8474439c986d2e065081eb5c6a0675310d5
PiotrSmoreda <piter.85magnum@gmail.com>
2023-11-08 12:57:47 +0100

    1

--- 08.11.2023/zad1.php
+++ 08.11.2023/zad1.php
@@ -1,1 +1,17 @@
-2131
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Porównanie zmiennych</title>
+</head>
+<body>
+    <?php
+    $zmienna1 = 25;
+    $zmienna2 = 15;
+    if ($zmienna1 > $zmienna2) {
+        echo "Większa wartość to: " . $zmienna1;
+    } else {
+        echo "Większa wartość to: " . $zmienna2;
+    }
+    ?>
+</body>
+</html>
