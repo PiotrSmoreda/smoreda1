ea7855d7903e33fb94b6db72339f9dba7dc92d60
PiotrSmoreda <piter.85magnum@gmail.com>
2023-11-16 14:54:48 +0100

    1

---
+++ 15.11.2023/zad3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>zad 3</title>
+</head>
+<body>
+    <?php
+$conn =  mysqli_connect("localhost", "root", '', "baza_danych");
+
+?>
+</body>
+</html>
